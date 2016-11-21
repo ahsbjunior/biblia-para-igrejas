ea51b2059832d4b0a570638977476dfc385e0e2f
Afonso Junior <ahsbjunior@gmail.com>
2016-11-21 21:12:42 -0200

    Alterando o TITLE das páginas; Adicionando JQUERY ao projeto

---
+++ js/bpi.js
@@ -0,0 +1,7 @@
+//var script = document.createElement('script');
+//script.src = 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js';
+//script.type = 'text/javascript';
+//document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function(){
+});
