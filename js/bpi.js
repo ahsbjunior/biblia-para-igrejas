bf5bfe5a39bc381378875d329e53e9a6510b9407
Afonso Junior <ahsbjunior@gmail.com>
2016-11-21 23:01:03 -0200

    adicionando função que seleciona versículo ao pressionar a tecla ‘V’. E com as setas laterais seleciona o próximo versículo

--- js/bpi.js
+++ js/bpi.js
@@ -4,4 +4,37 @@
 //document.getElementsByTagName('head')[0].appendChild(script);
 
 $(document).ready(function(){
+
+    var versiculoAtual = 1;
+    var ultimoVersiculo = document.getElementsByTagName('sup').length;
+
+    $(document).keydown(function () {
+        if (event.which == 118 || event.which == 86) {
+            var verse = prompt("Escolha o versículo", versiculoAtual);
+            if (verse != undefined && verse % 1 === 0) {
+                versiculoAtual = verse;
+                selecionaVersiculo();
+            }   
+        } else if (event.which == 37) { //esquerda
+            versiculoAtual--;
+            if (versiculoAtual < 1) 
+                versiculoAtual = 1;
+            selecionaVersiculo();
+        } else if (event.which == 39) { //direita
+            versiculoAtual++;
+            if (versiculoAtual > ultimoVersiculo) 
+                versiculoAtual = ultimoVersiculo;
+            selecionaVersiculo();
+        }  
+    });
+
+    function selecionaVersiculo() {
+        $('html, body').animate({
+            scrollTop: $('p.verse[verse=' + versiculoAtual + ']').offset().top - 100
+        }, 'slow');
+    }
+
+    //ao começar o documento, seleciono versículo 1
+    selecionaVersiculo();
 });
+
